2b8c69bd4cd20c689bd10f16e0fb926961fe598b
someonenoonew <teodorstefan815@yahoo.co.uk>
2025-06-17 20:46:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, request
+import requests
+import os
 
 app = Flask(__name__)
 
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
+
+@app.route('/')
+def home():
+    return 'Server activ!'
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    data = request.json
-    chat_id = data['message']['chat']['id']
-    print(f"Chat ID-ul grupului este: {chat_id}")
-    return "OK"
+    data = request.get_json()
+    mesaj = data.get('message', 'Alertă fără mesaj')
+
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        'chat_id': CHAT_ID,
+        'text': mesaj
+    }
+    r = requests.post(url, data=payload)
+    return 'Trimis', r.status_code
